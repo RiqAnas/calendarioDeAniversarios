1f6f061faf6b48f14578ff316ec40edc578be653
Henrique Santos <henriqanastacioc@gmail.com>
2025-12-07 12:19:26 -0300

    Adicionada barra das pastas

---
+++ lib/components/foldersViewList.dart
@@ -0,0 +1,107 @@
+import 'package:aniversariodois/core/models/folder.dart';
+import 'package:aniversariodois/core/models/person.dart';
+import 'package:aniversariodois/core/models/transitionArg.dart';
+import 'package:aniversariodois/core/services/folderService.dart';
+import 'package:aniversariodois/core/utils/colorsMap.dart';
+import 'package:aniversariodois/core/utils/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Foldersviewlist extends StatelessWidget {
+  final Person person;
+  final int selectedIndex;
+  final Function navigateAndSelect;
+
+  Foldersviewlist({
+    required this.person,
+    required this.selectedIndex,
+    required this.navigateAndSelect,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return StreamBuilder(
+      stream: Provider.of<Folderservice>(context, listen: false).folders,
+      builder: (context, folder) {
+        if (folder.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        }
+
+        if (!folder.hasData || folder.data!.isEmpty) {
+          return Container();
+        }
+
+        List<Folder> mainList = folder.data!
+            .where((fol) => fol.folderId == null)
+            .toList();
+
+        List<Folder> childrenList(String folderId) {
+          return folder.data!.where((fol) => fol.folderId == folderId).toList();
+        }
+
+        return ListView.builder(
+          padding: EdgeInsets.zero,
+          shrinkWrap: true,
+          itemCount: mainList.length,
+          physics: const NeverScrollableScrollPhysics(),
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                ListTile(
+                  leading: Icon(
+                    Icons.folder_outlined,
+                    color: Colorsmap.getColor(mainList[index].color),
+                  ),
+                  title: Text(mainList[index].name),
+                  selected: selectedIndex == 2,
+                  onTap: () => navigateAndSelect(
+                    context: context,
+                    index: 2,
+                    route: Routes.NOTEMENU,
+                    argument: Transitionarg(
+                      person: person,
+                      folder: mainList[index],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: MediaQuery.widthOf(context) * 0.05,
+                  ),
+                  child: ListView.builder(
+                    padding: EdgeInsets.zero,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: childrenList(mainList[index].id).length,
+                    itemBuilder: (context, subindex) {
+                      final children = childrenList(mainList[index].id);
+
+                      return ListTile(
+                        leading: Icon(
+                          Icons.folder_outlined,
+                          color: Colorsmap.getColor(children[subindex].color),
+                        ),
+                        title: Text(children[subindex].name),
+                        selected: selectedIndex == 2,
+                        onTap: () => navigateAndSelect(
+                          context: context,
+                          index: 2,
+                          route: Routes.NOTEMENU,
+                          argument: Transitionarg(
+                            person: person,
+                            folder: children[subindex],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+}
--- lib/components/sideBar.dart
+++ lib/components/sideBar.dart
@@ -1,6 +1,14 @@
+import 'package:aniversariodois/components/foldersViewList.dart';
+
+import 'package:aniversariodois/core/models/person.dart';
+import 'package:aniversariodois/core/models/transitionArg.dart';
+import 'package:aniversariodois/core/services/folderService.dart';
+import 'package:aniversariodois/core/services/personService.dart';
+
 import 'package:aniversariodois/core/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
 class Sidebar extends StatefulWidget {
   @override
@@ -9,8 +17,32 @@ class Sidebar extends StatefulWidget {
 
 class _SidebarState extends State<Sidebar> {
   int _selectedIndex = 0;
+  Person? _person;
+  ScrollController _scrollController = ScrollController();
 
-  void _navigateAndSelect(BuildContext context, int index, String route) {
+  void _getPerson() async {
+    try {
+      final person = await Provider.of<Personservice>(
+        context,
+        listen: false,
+      ).getPerson('home');
+
+      if (mounted) {
+        setState(() {
+          _person = person;
+        });
+      }
+    } catch (error) {
+      throw 'Erro ao tentar carregar pessoa';
+    }
+  }
+
+  void _navigateAndSelect({
+    required BuildContext context,
+    required int index,
+    required String route,
+    Transitionarg? argument,
+  }) {
     if (ModalRoute.of(context)?.settings.name == route) {
       Navigator.pop(context);
     } else {
@@ -18,13 +50,18 @@ class _SidebarState extends State<Sidebar> {
         _selectedIndex = index;
       });
       Navigator.pop(context);
-      Navigator.pushReplacementNamed(context, route);
+      if (argument == null) {
+        Navigator.pushReplacementNamed(context, route);
+      } else {
+        Navigator.pushNamed(context, route, arguments: argument);
+      }
     }
   }
 
   @override
   void initState() {
     super.initState();
+    _getPerson();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       final currentRoute = ModalRoute.of(context)?.settings.name;
       setState(() {
@@ -32,54 +69,102 @@ class _SidebarState extends State<Sidebar> {
           _selectedIndex = 0;
         } else if (currentRoute == Routes.SETTINGS) {
           _selectedIndex = 1;
+        } else if (currentRoute == Routes.NOTEMENU) {
+          _selectedIndex = 2;
         }
       });
     });
   }
 
   @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Drawer(
-      child: Column(
-        children: [
-          Container(
-            height: MediaQuery.heightOf(context) * 0.2,
-            color: Theme.of(context).colorScheme.primary,
-            alignment: Alignment.center,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.note,
-                  size: 40,
-                  color: Theme.of(context).colorScheme.surface,
+      child: Scrollbar(
+        controller: _scrollController,
+        radius: const Radius.circular(15),
+        thickness: 4,
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.heightOf(context) * 0.2,
+                color: Theme.of(context).colorScheme.primary,
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.note,
+                      size: 40,
+                      color: Theme.of(context).colorScheme.surface,
+                    ),
+                    const SizedBox(width: 10),
+                    Text(
+                      "NotesU",
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Theme.of(context).colorScheme.surface,
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(width: 10),
-                Text(
-                  "NotesU",
-                  style: TextStyle(
-                    fontSize: 40,
-                    color: Theme.of(context).colorScheme.surface,
-                  ),
+              ),
+              const SizedBox(height: 20),
+              ListTile(
+                leading: const Icon(Icons.note_outlined),
+                title: const Text("Início"),
+                selected: _selectedIndex == 0,
+                onTap: () => _navigateAndSelect(
+                  context: context,
+                  index: 0,
+                  route: Routes.HOME,
                 ),
-              ],
-            ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text("Configurações"),
+                selected: _selectedIndex == 1,
+                onTap: () => _navigateAndSelect(
+                  context: context,
+                  index: 1,
+                  route: Routes.SETTINGS,
+                ),
+              ),
+              ExpansionTile(
+                title: const Text("Pastas"),
+                leading: const Icon(Icons.folder_outlined),
+                onExpansionChanged: (bool expanded) {
+                  if (expanded) {
+                    Provider.of<Folderservice>(
+                      context,
+                      listen: false,
+                    ).loadFoldersperPerson('home');
+                  }
+                },
+                children: [
+                  const Divider(),
+
+                  if (_person == null)
+                    Container()
+                  else
+                    Foldersviewlist(
+                      person: _person!,
+                      selectedIndex: _selectedIndex,
+                      navigateAndSelect: _navigateAndSelect,
+                    ),
+                ],
+              ),
+            ],
           ),
-          const SizedBox(height: 20),
-          ListTile(
-            leading: const Icon(Icons.note_outlined),
-            title: const Text("Início"),
-            selected: _selectedIndex == 0,
-            onTap: () => _navigateAndSelect(context, 0, Routes.HOME),
-          ),
-          ListTile(
-            leading: const Icon(Icons.settings),
-            title: const Text("Configurações"),
-            selected: _selectedIndex == 1,
-            onTap: () => _navigateAndSelect(context, 1, Routes.SETTINGS),
-          ),
-        ],
+        ),
       ),
     );
   }
